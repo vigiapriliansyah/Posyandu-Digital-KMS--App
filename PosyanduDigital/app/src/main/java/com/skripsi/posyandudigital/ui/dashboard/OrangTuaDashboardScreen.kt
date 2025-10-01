c5a38078da252ffdeb4da072146725ddb28ed198
vigiapriliansyah <apriliansyahvigi@gmail.com>
2025-10-01 15:13:56 +0700

    update

--- PosyanduDigital/app/src/main/java/com/skripsi/posyandudigital/ui/dashboard/OrangTuaDashboardScreen.kt
+++ PosyanduDigital/app/src/main/java/com/skripsi/posyandudigital/ui/dashboard/OrangTuaDashboardScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
